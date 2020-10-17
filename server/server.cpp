4c219ad19feaed41af922b77f3ed4df429dbdfd4
root <root@dilesoft.fvds.ru>
2020-10-17 22:03:10 +0300

    uuid generate

--- server/server.cpp
+++ server/server.cpp
@@ -3,6 +3,42 @@
 #include <sstream>
 #include <ctime>
 
+#include <random>
+
+namespace uuid {
+    static std::random_device              rd;
+    static std::mt19937                    gen(rd());
+    static std::uniform_int_distribution<> dis(0, 15);
+    static std::uniform_int_distribution<> dis2(8, 11);
+
+    std::string generate_uuid_v4() {
+        std::stringstream ss;
+        int i;
+        ss << std::hex;
+        for (i = 0; i < 8; i++) {
+            ss << dis(gen);
+        }
+        ss << "-";
+        for (i = 0; i < 4; i++) {
+            ss << dis(gen);
+        }
+        ss << "-4";
+        for (i = 0; i < 3; i++) {
+            ss << dis(gen);
+        }
+        ss << "-";
+        ss << dis2(gen);
+        for (i = 0; i < 3; i++) {
+            ss << dis(gen);
+        }
+        ss << "-";
+        for (i = 0; i < 12; i++) {
+            ss << dis(gen);
+        };
+        return ss.str();
+    }
+}
+
 #define LAG -3000
 //#define EVENTS_LOG
 #define SERVER_VERSION 1
@@ -1784,6 +1820,7 @@ int Server::quant() {
 	// fout < "Quant: " <= frame < "\t" <= SDL_GetTicks() < "\n";
 	//std::cout << "Test" << SDL_GetTicks() << "\n";
 	if (std::time(0) - next_hub >= 10) {
+		std::cout << uuid::generate_uuid_v4() << std::endl;
 		next_hub = std::time(0);
 		std::ostringstream s1;
 		s1 << "curl -v --header 'Content-Type: application/json' --data '{\"username\": \"" << games.size() << "\", \"password\":\"xyz\"}' http://vangers.dilesoft.ru/server/test.php";
