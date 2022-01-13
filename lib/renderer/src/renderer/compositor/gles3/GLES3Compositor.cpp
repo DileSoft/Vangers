4c9338f28f082b7ec73cc4910f057faa42202165
Nikita <lpenguin@yandex.ru>
2022-01-13 23:25:20 +0300

    Removed glDebug from xgraph

--- lib/renderer/src/renderer/compositor/gles3/GLES3Compositor.cpp
+++ lib/renderer/src/renderer/compositor/gles3/GLES3Compositor.cpp
@@ -45,7 +45,85 @@ const char* fs_code = R"(
 )";
 
 
+const char * get_debug_type_str(GLenum type){
+	switch(type){
+	case GL_DEBUG_TYPE_ERROR:
+		return "ERROR";
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+		return "DEPRECATED_BEHAVIOR";
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+		return "UNDEFINED_BEHAVIOR";
+	case GL_DEBUG_TYPE_PORTABILITY:
+		return "PORTABILITY";
+	case GL_DEBUG_TYPE_PERFORMANCE:
+		return "PERFORMANCE";
+	case GL_DEBUG_TYPE_OTHER:
+		return "OTHER";
+	case GL_DEBUG_TYPE_MARKER:
+		return "MARKER";
+	case GL_DEBUG_TYPE_POP_GROUP:
+		return "POP_GROUP";
+	case GL_DEBUG_TYPE_PUSH_GROUP:
+		return "PUSH_GROUP";
+	default:
+		return "Unknown";
+	}
+}
 
+const char* get_debug_severity_str(GLenum severity){
+	switch(severity){
+	case GL_DEBUG_SEVERITY_HIGH:
+		return "HIGH";
+	case GL_DEBUG_SEVERITY_LOW:
+		return "LOW";
+	case GL_DEBUG_SEVERITY_MEDIUM:
+		return "MEDIUM";
+	case GL_DEBUG_SEVERITY_NOTIFICATION:
+		return "NOTIFICATION";
+	default:
+		return "Unknown";
+	}
+}
+
+const char* get_debug_source_str(GLenum source){
+	switch(source){
+	case GL_DEBUG_SOURCE_API:
+		return "API";
+	case GL_DEBUG_SOURCE_APPLICATION:
+		return "APPLICATION";
+	case GL_DEBUG_SOURCE_OTHER:
+		return "OTHER";
+	case GL_DEBUG_SOURCE_SHADER_COMPILER:
+		return "SHADER_COMPILER";
+	case GL_DEBUG_SOURCE_THIRD_PARTY:
+		return "THIRD_PARTY";
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+		return "WINDOW_SYSTEM";
+	default:
+		return "Unknown";
+	}
+}
+
+void DebugCallbackARB(GLenum source,
+                      GLenum type,
+                      GLuint id,
+                      GLenum severity,
+                      GLsizei length,
+                      const GLchar* message,
+                      const GLvoid* userParam) {
+	if(userParam != nullptr ||
+	   type == GL_DEBUG_TYPE_ERROR)
+	{
+		std::cout << "GL Debug Message:"
+		          << std::endl
+		          << "\tseverity=" << get_debug_severity_str(severity)
+		          << ", type=" << get_debug_type_str(type)
+		          << ", source=" << get_debug_source_str(source)
+		          << std::endl;
+
+		std::cout << "\tmessage=" << std::string(message).substr(0, length) << std::endl;
+	}
+}
 
 GLES3Compositor::GLES3Compositor(int32_t screen_width, int32_t screen_height, GLADloadproc loadproc)
 	: _screen_width(screen_width)
@@ -58,6 +136,10 @@ GLES3Compositor::GLES3Compositor(int32_t screen_width, int32_t screen_height, GL
 	, _previous_vao(0)
 {
 	gladLoadGLES2Loader(loadproc);
+
+	glEnable(GL_DEBUG_OUTPUT);
+	// pass here any value to catch only every debug message
+	glDebugMessageCallback(&DebugCallbackARB, nullptr);
 
 	_texture_shader->initialize(vs_code, fs_code);
 	_texture_shader->use();
