6aac86a950bdd458915fcb4e192b9e0de95bbc27
Nikita <lpenguin@yandex.ru>
2022-01-13 23:30:32 +0300

    Minor: sokol_gfx_settings

--- lib/renderer/src/renderer/lib/sokol_gfx_settings.h
+++ lib/renderer/src/renderer/lib/sokol_gfx_settings.h
@@ -1,7 +1,6 @@
 #ifndef SOKOL_GFX_SETTINGS_H
 #define SOKOL_GFX_SETTINGS_H
 
-// TODO: take this from CMakeLists.txt
-#define SOKOL_GLES3
+#define SOKOL_GLCORE33
 #define SOKOL_DEBUG
 #endif // SOKOL_GFX_SETTINGS_H
