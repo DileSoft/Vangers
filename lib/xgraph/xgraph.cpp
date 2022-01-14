fca46cd1b687f6dacb29051f1da40ea355768f1e
Nikita <lpenguin@yandex.ru>
2022-01-14 13:59:00 +0300

    SDL_HINT_OPENGL_ES_DRIVER

--- lib/xgraph/xgraph.cpp
+++ lib/xgraph/xgraph.cpp
@@ -148,6 +148,7 @@ int XGR_Screen::init(int x,int y,int flags_in)
 		SDL_DestroyWindow(sdlWindow);
 	}
 
+	SDL_SetHint(SDL_HINT_OPENGL_ES_DRIVER, "1");
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
