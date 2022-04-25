4e415f28d6192df53ea22ecfec6e27603809e095
Alexander Guryanov <caiiiycuk@gmail.com>
2022-04-25 16:26:30 +0700

    Allow to resize window
    Allow to resize window

--- lib/xgraph/xgraph.cpp
+++ lib/xgraph/xgraph.cpp
@@ -155,7 +155,7 @@ int XGR_Screen::init(int x,int y,int flags_in)
 			ErrH.Abort(SDL_GetError(),XERR_USER, 0);
 		}
 	} else {
-		if (SDL_CreateWindowAndRenderer(x, y, 0, &sdlWindow, &sdlRenderer) < 0) {
+		if (SDL_CreateWindowAndRenderer(x, y, SDL_WINDOW_RESIZABLE, &sdlWindow, &sdlRenderer) < 0) {
 			std::cout<<"ERROR2"<<std::endl;
 			ErrH.Abort(SDL_GetError(),XERR_USER, 0);
 		}
